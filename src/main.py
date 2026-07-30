2e4f645aa1c5a25cec4f55baef12e085c7d4919a
RazeIDK <razeidk.code@proton.me>
2026-07-30 19:29:20 +0300

    Start vars protect

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 import ast
+import os
+import variables
+
 
 def main():
-	tree = ast.parse()
+	path_to_file = input("Path to file: ")
+	tree = ast.parse(path_to_file)
+
+
+if __name__ == "__main__":
+	main()
